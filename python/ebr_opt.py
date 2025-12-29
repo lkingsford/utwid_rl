8c456ce7a3861763b76f2542695dab3d9683ea1b
Lachlan Kingsford <lachlan@nerdygentleman.com>
2025-12-30 09:20:34 +1100

    EBR Opt Progress

--- python/ebr_opt.py
+++ python/ebr_opt.py
@@ -4,7 +4,13 @@ import optuna
 
 import mon2y
 
-MAX_REV = 50
+MAX_REVENUE = 50
+
+MIN_BUILD_COST = 0
+MAX_BUILD_COST = 20
+
+MIN_ADDITIONAL_COST = 0
+MAX_ADDITIONAL_COST = 20
 
 MAX_BOND_COUNT = 50
 MIN_BOND_FACE_STEP = 0
@@ -13,7 +19,26 @@ MIN_BOND_COUPON_STEP = 0
 MAX_BOND_COUPON_STEP = 10
 
 def suggest_revenue(feature: str, trial: optuna.Trial) -> List[int]:
-    return [trial.suggest_int(f"feature_rev_rnd_{i}", 0, 50) for i in range(0, 6)]
+    # Revenue goes for two rounds each time
+    return [trial.suggest_int(f"{feature}_rev_rnd_{i}", 0, MAX_REVENUE) for i in range(0, 3) for _ in (0, 1)]
+
+Terrain = TypedDict("Terrain", {"build_cost": int, "revenue": List[int]})
+
+def suggest_terrain(terrain: str, trial: optuna.Trial) -> Terrain:
+    return {
+        "build_cost": trial.suggest_int(f"{terrain}_build_cost", MIN_BUILD_COST, MAX_BUILD_COST),
+        "revenue": suggest_revenue(terrain, trial)
+    }
+
+Feature = TypedDict("Feature", {"feature_type": str, "location_name": str, "revenue": List[int], "additional_cost": int})
+
+def suggest_modified_feature(feature: Feature, trial: optuna.Trial) -> Feature:
+    return {
+        "feature_type": feature["feature_type"],
+        "location_name": feature["location_name"],
+        "revenue": suggest_revenue(feature["location_name"], trial),
+        "additional_cost": trial.suggest_int(f"{feature}_additional_cost", MIN_ADDITIONAL_COST, MAX_ADDITIONAL_COST)
+    }
 
 Bond = TypedDict("Bond", {"face_value": int, "coupon": int})
 
@@ -34,4 +59,14 @@ def suggest_bonds(trial: optuna.Trial) -> List[Bond]:
         )
 
     return bonds
+
+CompanyFixedDetail = TypedDict("CompanyFixedDetail", {
+    "starting": List[int],
+    "private": bool,
+    "stock_available": int,
+    "track_available": int,
+    "initial_treasury": int,
+    "initial_interest": int})
+
+
 
